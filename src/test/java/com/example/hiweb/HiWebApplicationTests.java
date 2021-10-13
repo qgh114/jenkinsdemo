793cb496a311d778c8bbc0a6aeb10cf95abcc3a5
Stanley Lante Lamptey <36686061+qgh114@users.noreply.github.com>
2021-10-13 10:26:45 +0200

    Add files via upload

---
+++ src/test/java/com/example/hiweb/HiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
